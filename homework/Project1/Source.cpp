9dcf0f54fb35d6e37c1179dfa767bd7a2de51d66
Stefysichka <114502048+Stefysichka@users.noreply.github.com>
2022-10-12 14:03:28 +0300

    Update Source.cpp

--- homework/Project1/Source.cpp
+++ homework/Project1/Source.cpp
@@ -4,18 +4,7 @@
 using namespace std;
 int main()
 {
-	float radius;
-	cout << "write the radius of the circle" << endl;
-	cin >> radius;
-	float area;
-	area = radius * radius * 3.14;
-	cout << "area=" << area << endl;
-	float length;
-	length = 2 * 3.14 * radius;
-	cout << "length=" << length << endl;
-	float volume;
-	volume = static_cast<double>(4) / 3 * 3.14 * radius * radius * radius;
-	cout << "volume=" << volume << endl;
+
 	cout << " write your 2 most favourite numbers" << endl;
 	int number1;
 	cin >> number1;
@@ -27,6 +16,7 @@ int main()
 	int geometricmean;
 	geometricmean = sqrt(number1 * number2);
 	cout << "geometric mean=" << geometricmean << endl;
+	\\Обчислити середнє арифметичне, середнє геометричне двох заданих натуральних чисел.
 
 
 
