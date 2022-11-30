ce29e2f37eeabd2b159b47db7f77e13024bfe6bb
Stefysichka <srefaniamavdruk@gmail.com>
2022-11-30 13:26:36 +0200

    add "practice classes 2" new progects

---
+++ homework/classes module/classes module/Source.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+/*Визначити тип пряма на площині  за трьома дійсними числами.
+Крім необхідних методів (конструкторів, введення-виведення,...) 
+визначити метод знаходження точки перетину двох прямих, метод знаходження 
+кута перетину двох прямих, методи перевірки, чи належить точка заданій прямій, 
+чи прямі паралельні. Ввести дані про 10 прямих. Визначити 1) для кожної прямої 
+список прямих, що з нею перетинаються та координати точок перетину 2) скільки з 
+них паралельні осі ОХ і скільки з них перетинаються з нею під прямим кутом.*/
+//ax+by+c=0
+class Line {
+private:
+	double a, b, c;
+public:
+	Line() :a(0), b(0), c(0) {}
+	Line(const Line& p/*з неї робимо копію*/) :a(p.a), b(p.b), c(p.c) {}
+	friend istream& operator>>(istream& is, Line& line) {
+		is >> line.a >> line.b >> line.c;
+		return is;
+	}
+	friend ostream& operator<<(ostream& os, Line& line) {
+		os << line.a << 'x' << '+' << line.b << 'y' << '+' << line.c << '=' << '0';
+
+			return os;
+	}
+	struct Point {
+		double x, y;
+	};
+	Point getInterception(Line& line) /*перевіряєм чи перетинаються*/ {
+		double bottom/*знаменник, внизу у формулі,коли шукаєм перетин прямих*/ = a * line.b - line.a * b;
+		double x = (b * line.c - line.b * c) / bottom;
+		double y = (c * line.a - line.c * a) * bottom;
+		//line.шось координати 2 прямої
+		Point p;
+		p.x = x;
+		p.y = y;
+		return p;
+
+		bool isinteger(Line &line) {
+			double bottom = a * line.b - line.a * b;
+			if (bottom == 0) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
+	}
+	
+};
---
+++ homework/lecture classes/lecture classes/Source.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+/*1*//*class Room {
+public:
+	double length;
+	double breadth;
+	double height;
+	double calculateArea() {
+		return length * breadth;
+	}
+	double calculateVolume() {
+		return length * breadth * height;
+	}
+};
+int main() {
+	Room room1;
+	room1.length = 42.5;
+	room1.breadth = 30.8;
+	room1.height = 19.2;
+	cout << "Area of Room" << room1.calculateArea();
+	cout << "Volume of Room" << room1.calculateVolume();
+	return 0;
+}*/
+/*aбо*/
+/*class Room {
+private:
+	double length;
+	double breadth;
+	double height;
+public:
+	void setData(double len, double brth, double hgt) {
+		length = len;
+		breadth = brth;
+		height = hgt;
+	}
+	double calculateArea() {
+		return length * breadth;
+	}
+	double calculateVolume() {
+		return length * breadth * height;
+	}
+};
+int main() {
+	Room room1;
+	room1.setData(42.5, 30.8, 19.2);
+	cout << room1.calculateArea();
+	cout << room1.calculateVolume();
+}*/
+/*2*/
+/*class Point {
+private:
+	int x;
+	int y;
+public:
+	Point() { x = 0; y = 0; }//дефолтний
+	Point(int xx,int yy):x(xx),y(yy){}
+	Point(const Point& p): x(p.x), y(p.y){}
+};
+int main() {
+	Point p1;//викд дефолтний
+	Point p2(0, 0);//викл з параметром
+	Point p3(p2);//копіювання
+}*/
+/*3*/
+//Point.h
+/*class Point {
+protected:
+	int x;
+	int y;
+public:
+	Point() :x(0), y(0) {}
+	Point(int xx,int yy):x(xx), y(yy){}
+	Point(const Point& p) : x(p.x), y(p.y) {}
+	void print()const {
+		cout << "Point(" << x << ',' << y << ')';
+	}
+	double dist(const Point&)const;//відстань між 2 точками, передаєм 1 точку , бо 1 вже є 
+	Point operator+(const Point& p)const {
+		return Point(x + p.x, y + p.y);
+	}//можна додати точку, аналогічно такому P1=p2+p3; p2.operator+(p3)
+};
+void main() {
+...
+}*/
+class Values {
+public:
+	//оголошення статичних полів
+	static int how_many;
+	static const float pi;
+	//ініціалізація статичних полів
+	static const int size = 100;
+	//збільшуєм лічильник при утворенні нового об'єкта 
+	Values() { how_many++; };
+};
+void main() {
+	//використання полів без об'єктів класу
+	cout << "pi=" << Values::pi << endl;
+	cout << "size=" << Values::size << endl;
+	//утворюємо 4 об'єкта
+	Values v1, v2, v3, v4;
+	cout << "how_many=" << Values::how_many << endl;
+
+}
