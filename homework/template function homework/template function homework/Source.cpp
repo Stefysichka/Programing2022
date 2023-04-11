da5c0f710b3000d11fedbbf19e886753b1603c2d
Stefysichka <srefaniamavdruk@gmail.com>
2023-04-11 14:19:44 +0300

    Add "template function" new projects

---
+++ homework/template function homework/template function homework/Source.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+template <typename T>
+void sort(T a[], int n, int i) {
+	for (int j = 0; j < i; j++) {
+		for (int k = j + i; k < i; k++) {
+			if (a[j] > a[k]) {
+				T temp = a[j];
+				a[j] = a[k];
+				a[k] = temp;
+			}
+		}
+	}
+}
+int main() {
+	const int n = 5;
+	double a1[n]= {3.2, 4.4, 3, 5.1, 48.3};
+	string a2[n] = { "hello","Stefik","Name","cat","dog" };
+	sort(a1, n, 3);
+	sort(a2, n, 2);
+	for (int i = 0; i < n; ++i) {
+		cout << a1[i];
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++) {
+		cout << a2[i];
+	}
+	cout << endl;
+	return 0;
+}
